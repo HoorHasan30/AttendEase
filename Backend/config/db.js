58491271bdfd6dfaab011bb171e6f089a10192e5
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-24 12:57:25 +0300

    Fix: Add DNS

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose")
 
+const dns = require("dns")
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+
 async function connectToDB(){ //connection to the database
     try{
         const connection = await mongoose.connect(process.env.MONGODB_URI)
