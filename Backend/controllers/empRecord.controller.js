56eeb0f70405dfe499e8b5ae351f55b79e36b9ff
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-26 11:19:17 +0300

    Feat: Add Dashboard Route

--- Backend/controllers/empRecord.controller.js
+++ Backend/controllers/empRecord.controller.js
@@ -6,7 +6,7 @@ async function getAllCompanyRecords(req, res) {
 
         // get company punches Id
         const companyPunches = await Punch.find({ company: req.user.company }).select('_id')
-        const punchIds = companyPunches.map( p => p._id)
+        const punchIds = companyPunches.map(p => p._id)
 
         // get the records
         const foundRecords = await EmployeeRecord.find({ punchRecord: { $in: punchIds } }).populate('punchRecord')
@@ -22,6 +22,60 @@ async function getAllCompanyRecords(req, res) {
     }
 }
 
+async function getDashboard(req, res) {
+    try {
+        const { from, to } = req.body
+
+        if (!from || !to){
+            return res.status(400).json({ message: "Both dates are required" })
+        }
+
+        // get punches filtered by the dates
+        const punches = await Punch.find({ company: req.user.company, date: { $gte: new Date(from), $lte: new Date(to)} })
+
+        // get records
+        const records = await EmployeeRecord.find({ punchRecord: { $in: punchIds } }).populate('punchRecord')
+
+        // collecting data
+        let totalShortage = 0
+        let totalOvertime = 0
+
+        // grouped by day(date)
+        const byDay = {}
+
+        records.forEach( r => {
+            totalShortage += r.shortage
+            totalOvertime += r.overtime
+
+            const recDate = r.punchRecord.date.toISOString().split('T')[0]
+
+            // if the day is not in the object
+            if(!byDay[recDate]){
+                byDay[recDate] = { date: recDate, shortage: 0, overtime: 0 }
+            }
+
+            byDay[recDate].shortage += r.shortage
+            byDay[recDate].overtime += r.overtime
+        })
+        
+        const trendChartData = Object.values(byDay).sort((a, b) => a.date.localeCompare(b.date))
+
+        res.status(200).json({
+            totalShortage,
+            totalOvertime,
+            trendChartData
+        })
+    }
+    catch (err) {
+        if (err.name === 'ValidationError') {
+            return res.status(400).json({ message: err.message })
+        }
+
+        res.status(500).json({ message: err.message })
+    }
+}
+
 module.exports = {
-    getAllCompanyRecords
+    getAllCompanyRecords,
+    getDashboard
 }
--- Backend/routes/empRecord.routes.js
+++ Backend/routes/empRecord.routes.js
@@ -5,5 +5,6 @@ const verifyToken = require("../middleware/verifyToken");
 const isHR = require('../middleware/isHR')
 
 router.get('/', verifyToken, isHR, empRecordsController.getAllCompanyRecords)
+router.post('/dashboard', verifyToken, isHR, empRecordsController.getDashboard)
 
 module.exports = router;
