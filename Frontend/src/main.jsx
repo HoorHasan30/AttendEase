09a77546cad5b91f2f7349525b743030f6f928cf
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-27 18:31:16 +0300

    Style: Override Bootstrap colors

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { AuthProvider } from "./context/AuthContext.jsx";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/bootstrapThemeOverride.scss'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
