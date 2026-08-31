c224335b877f86c8384e47dc00fd741efb62b81c
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-31 17:16:47 +0300

    Feat: Add TotalEmployees in getDashboard()

--- Backend/controllers/empRecord.controller.js
+++ Backend/controllers/empRecord.controller.js
@@ -32,16 +32,23 @@ async function getDashboard(req, res) {
 
         // get punches filtered by the dates
         const punches = await Punch.find({ company: req.user.company, date: { $gte: new Date(from), $lte: new Date(to)} })
+        const punchIds = punches.map(p => p._id)
 
         // get records
         const records = await EmployeeRecord.find({ punchRecord: { $in: punchIds } }).populate('punchRecord')
 
+        const totalEmployees = await Punch.distinct('employeeId', {
+            company: req.user.company,
+            date: { $gte: new Date(from), $lte: new Date(to) }
+        })
+        
         // collecting data
         let totalShortage = 0
         let totalOvertime = 0
 
         // grouped by day(date)
         const byDay = {}
+
 
         records.forEach( r => {
             totalShortage += r.shortage
@@ -61,6 +68,7 @@ async function getDashboard(req, res) {
         const trendChartData = Object.values(byDay).sort((a, b) => a.date.localeCompare(b.date))
 
         res.status(200).json({
+            totalEmployees: totalEmployees.length,
             totalShortage,
             totalOvertime,
             trendChartData
